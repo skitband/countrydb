7ee3f3c4f77c7cc61af4611e4f74f507c8528182
Sergio Lio <sergio.lio@bcstechnology.com.au>
2022-10-08 11:38:16 +0800

    make it better

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -10,7 +10,7 @@ const Countries = ({ currentItems, selectCountry }) => {
       <h4 className="my-1">Countries List</h4>
       {currentItems &&
         currentItems.map((item, index) => (
-            <a key={index} href="#" className="list-group-item list-group-item-action lh-sm fw-semibold border-0 bg-container p-0" aria-current="true" onClick={() => { handleOnClick(item.name.official) }}>
+            <a key={index} href="#" className="list-group-item list-group-item-action lh-sm fw-semibold border-0 bg-container" aria-current="true" onClick={() => { handleOnClick(item.name.official) }}>
               {item.name.official}
             </a>
         ))}
